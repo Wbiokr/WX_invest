9bbc0cd05a48fc29a2e20dfd885985dbac4bdbc9
wbiokr <Wbiokr@hotmail.com>
2018-06-28 13:53:44 +0800

    添加平台记录

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,3 +1,5 @@
+const {api} = require('../../utils/config')
+
 Page({
   data:{
     name:'',
@@ -5,7 +7,7 @@ Page({
     rateAll:'',
     rateHas:'',
     startTime:'',
-    duration:'',
+    duration:30,
     phone:'',
     card:'',
     arrPhone:[
@@ -19,6 +21,96 @@ Page({
     visiblePhone:false,
     visibleCard:false,
     isLoading:false,
+    visibleUpdate:false,
+  },
+  update(){
+    this.setData({
+      visibleUpdate:false,
+    })
+    this.addInvestItem(true)
+  },
+  cancelUpdate(){
+    this.setData({
+      visibleUpdate:false
+    })
+  },
+  addInvestItem(isUpdate=false){
+
+    if(this.data.isLoading){
+      return ;
+    }
+    const data={isUpdate}
+    if(!this.data.name){
+      wx.showToast({title:'请输入正确name',icon:'none'});
+      return ;
+    }   
+    data.name=this.data.name;
+    if(!this.data.cash){
+      wx.showToast({title:'请输入正确的cash',icon:'none'});
+      return ;
+    }
+    data.cash=this.data.cash;
+    if(!this.data.rateAll){
+      wx.showToast({title:'请输入正确的rateAll',icon:'none'});
+      return ;
+    }
+    data.rateAll=this.data.rateAll
+    if(!this.data.rateHas){
+      wx.showToast({title:'请输入正确的rateHas',icon:'none'});
+      return ;
+    }
+    data.rateHas=this.data.rateHas;
+    if(!this.data.startTime){
+      wx.showToast({title:'请输入正确的startTime',icon:'none'});
+      return ;
+    }
+    data.startTime=new Date(this.data.startTime).getTime();
+    if(!this.data.duration){
+      wx.showToast({title:'请输入正确的duration',icon:'none'});
+      return ;
+    }
+    data.duration=this.data.duration;
+    if(!this.data.phone){
+      wx.showToast({title:'请输入正确的phone',icon:'none'});
+      return ;
+    }
+    data.phone=this.data.phone;
+    if(!this.data.card){
+      wx.showToast({title:'请输入正确的card',icon:'none'});
+      return ;
+    }
+    data.card=this.data.card;
+    this.setData({isLoading:true})
+
+    wx.request({
+      url:`${api}invest/edit`,
+      method:'post',
+      data,
+      dataType:'json',
+      header:{
+        'content-type':'application/x-www-form-urlencoded'
+      },
+      success:(res)=>{
+        console.log(res)
+        const data=res.data;
+        if(data.code==1){
+          wx.showToast({title:data.msg,icon:'none'})
+        }else if(data.code==-1){
+          wx.showToast({title:data.msg,icon:'none',duration:500})
+          setTimeout(()=>{
+            this.setData({
+              visibleUpdate:true
+            })
+          },500)
+        }
+      },
+      error(err){
+        // wx.showToast({})
+      },
+      complete:()=>{
+        this.setData({isLoading:false})
+      }
+    })
   },
   changeName(e){
     console.log(e)
