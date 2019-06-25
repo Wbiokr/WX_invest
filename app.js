8e69bc71e6c4afe8e9867c54673dd3b072c5dab6
陳博先 <wbk.chen@transn.com>
2019-06-25 14:17:46 +0800

    wxs

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
 App({
-
-  onLaunch: function (params) {
-    console.log('launch',params)
-  },
-
-  onShow: function (options) {
-    console.log('show',options)
-    this.hello()
-  },
-
-  onHide: function () {
-    console.log('hide')    
-  },
-
-  onError: function (msg) {
-    console.log(`err:${msg}`)    
-  },
-
-  onPageNotFound(options){
-    console.log(`404:${options}`)
-  },
-
-  hello(){
-    console.log('hello world !')
+  onLaunch(params){
+    console.log(params)
   }
 })
